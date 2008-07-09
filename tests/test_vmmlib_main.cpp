9ca33afcd5a36c9d24f9384c6a1ae48813a40f3a
l3m <l3m@4d770e62-bb1e-0410-ad62-f2382d3e8790>
2008-07-09 00:04:51 +0000

    forgot to add new test_main cpp
    git-svn-id: https://vmmlib.svn.sourceforge.net/svnroot/vmmlib/trunk@141 4d770e62-bb1e-0410-ad62-f2382d3e8790

---
+++ tests/test_vmmlib_main.cpp
@@ -0,0 +1,21 @@
+
+#include "matrix_test.hpp"
+#include "qr_decomposition_test.hpp"
+
+
+#include <iostream>
+
+void
+run_and_log( vmml::unit_test& test )
+{
+    test.run();
+    std::cout << test << std::endl; 
+}
+
+int
+main( int argc, const char* argv[] )
+{
+    vmml::matrix_test matrix_test_;
+    run_and_log( matrix_test_ );
+
+}

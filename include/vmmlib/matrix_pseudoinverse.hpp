cd1c46825546cfcfb7922a0cb8c189bb5e6ac2e6
susuter <susuter@4d770e62-bb1e-0410-ad62-f2382d3e8790>
2010-10-25 11:07:03 +0000

    - corrected matrix pseudoinverse test
    git-svn-id: https://vmmlib.svn.sourceforge.net/svnroot/vmmlib/trunk@325 4d770e62-bb1e-0410-ad62-f2382d3e8790

--- include/vmmlib/matrix_pseudoinverse.hpp
+++ include/vmmlib/matrix_pseudoinverse.hpp
@@ -30,21 +30,20 @@ namespace vmml
 		void operator()( const T& input, T& pseudoinverse_transposed, 
 						typename T::value_type tolerance = std::numeric_limits< typename T::value_type >::epsilon() )
 		{
-			//const size_t M 			= T::ROWS;
-			//const size_t N 			= T::COLS;
-			//typedef T::value_type myfloat;		
 
+			
 			if ( T::ROWS < T::COLS ) {
 				VMMLIB_ERROR( "matrix compute_pseudoinverse - number of matrix rows have to be greater or equal to number of matrix columns.", VMMLIB_HERE );
 			}
 			
 						
 			// perform an SVD on the matrix to get the singular values
-			lapack_svd< T::ROWS, T::COLS, typename T::value_type > svd;
-			matrix< T::ROWS, T::COLS, typename T::value_type > U;
-			vector< T::COLS, typename T::value_type > sigmas;
-			matrix< T::COLS, T::COLS, typename T::value_type > Vt;
+			lapack_svd< T::ROWS, T::COLS, double > svd;
+			matrix< T::ROWS, T::COLS, double > U;
+			vector< T::COLS, double > sigmas;
+			matrix< T::COLS, T::COLS, double > Vt;
 			bool ok = svd.compute( input, U, sigmas, Vt ); 
+						
 			
 			if ( !ok ) {
 				VMMLIB_ERROR( "matrix compute_pseudoinverse - problem with lapack svd.", VMMLIB_HERE );
@@ -52,7 +51,7 @@ namespace vmml
 		
 
 			// get the number of significant singular, i.e., values which are above the tolerance value
-			typename vector< T::COLS, typename T::value_type >::const_iterator it = sigmas.begin() , it_end = sigmas.end();
+			typename vector< T::COLS, double >::const_iterator it = sigmas.begin() , it_end = sigmas.end();
 			size_t num_sigmas = 0;
 			for( ; it != it_end; ++it )
 			{
@@ -65,23 +64,25 @@ namespace vmml
 			//compute inverse with all the significant inverse singular values
 			matrix< T::COLS, T::ROWS, typename T::value_type > result;
 			result.zero();
-			matrix< T::COLS, T::ROWS, typename T::value_type > tmp;
+			matrix< T::COLS, T::ROWS, double > tmp;
+			double sigma_inv = 0;
 			if ( num_sigmas >= 1 ) {
 				
 				it = sigmas.begin();
 				for( size_t i = 0 ;  i < num_sigmas && it != it_end; ++it, ++i ) {
-					matrix< 1, T::COLS, typename T::value_type > vt_i;
+					matrix< 1, T::COLS, double > vt_i;
 					Vt.get_sub_matrix(vt_i, i);
-					matrix< T::COLS, 1, typename T::value_type > v_i = transpose(vt_i);
-					matrix< T::ROWS, 1, typename T::value_type > u_i;
+					matrix< T::COLS, 1, double > v_i = transpose(vt_i);
+					matrix< T::ROWS, 1, double > u_i;
 					U.get_sub_matrix(u_i, 0, i); 
-					matrix< 1, T::ROWS, typename T::value_type > ut_i = transpose(u_i);
+					matrix< 1, T::ROWS, double > ut_i = transpose(u_i);
 					
 					//build outer product of v1_i and ut_i
 					tmp.multiply(v_i, ut_i);
 					
 					//sigma value inverted: 1 / *it;
-					tmp *= (1 / *it );
+					//sigma_inv = (1 / *it );
+					tmp *= *it ;
 					result += tmp;
 					
 				}				
--- tests/matrix_pseudoinverse_test.cpp
+++ tests/matrix_pseudoinverse_test.cpp
@@ -22,24 +22,29 @@ namespace vmml
 			0.4218, 0.9157, 0.7922, 0.9595, 
 			0.6557, 0.0357, 0.8491, 0.9340};
 		input.set( data, data + 24);
-		matrix< 6, 4, float > pseudoinverse_transposed_control;
+		matrix< 4, 6, float > pseudoinverse_control;
 		double data2[] = { 0.1880, 0.1967, -0.4289, 0.1703, 
 			0.5039, -0.5719, -0.2487, 0.2746, 
 			0.3297, 0.1597, -0.4866, 0.1270, 
 			0.6085, 0.2728, 0.5149, -0.9327, 
 			-0.9104, 0.6751, 0.6790, 0.1391, 
 			0.0213, -0.7584, 0.2949, 0.6102 };
-		pseudoinverse_transposed_control.set( data2, data2 + 24);
+		
+		double data3[] = {
+			0.8147, 0.6324, 0.9575, 0.9572, 0.4218, 0.6557,
+			0.9058, 0.0975, 0.9649, 0.4854, 0.9157, 0.0357,
+			0.1270, 0.2785, 0.1576, 0.8003, 0.7922, 0.8491,
+			0.9134, 0.5469, 0.9706, 0.1419, 0.9595,  0.9340
+		};
+		
+		pseudoinverse_control.set( data3, data3 + 24);
 		
 		matrix< 6, 4, float > pseudoinverse_transposed;
                 compute_pinv( input, pseudoinverse_transposed );
 
-
-                matrix< 4, 6, float > pseudoinverse = transpose( pseudoinverse_transposed );
-                matrix< 6, 6, float > control;
-                control.multiply( input, pseudoinverse);
+		matrix< 4, 6, float > pseudoinverse = transpose( pseudoinverse_transposed );
 		
-		if ( pseudoinverse_transposed_control.equals( pseudoinverse_transposed, 0.1 ))
+		if ( pseudoinverse_control.equals( pseudoinverse, 1e-4 ))
 		{	
 			log( "matrix compute pseudo inverse ", true  );
 		} else
@@ -47,9 +52,9 @@ namespace vmml
 			std::stringstream error;
 			error 
 			<< "matrix compute pseudo inverse: " << std::endl
-			<< "inverse matrix (transposed) should be: " << std::endl << pseudoinverse_transposed_control << std::endl
-			<< "inverse matrix (transposed) is: " << std::endl << pseudoinverse_transposed << std::endl
-			<< "identiy matrix (control) is: " << std::endl << control << std::endl;
+			<< "input is: " << std::endl << input << std::endl
+			<< "inverse matrix should be: " << std::endl << pseudoinverse_control << std::endl
+			<< "inverse matrix is: " << std::endl << pseudoinverse << std::endl;
 			
 			log_error( error.str() );
 		}

06e456b7eb0e45e21e69a3126e4450055f9d9cbe
susuter <susuter@4d770e62-bb1e-0410-ad62-f2382d3e8790>
2011-09-30 09:08:22 +0000

    - matrix pseudo inverse: for large data sets
    git-svn-id: https://vmmlib.svn.sourceforge.net/svnroot/vmmlib/trunk@517 4d770e62-bb1e-0410-ad62-f2382d3e8790

--- include/vmmlib/matrix_pseudoinverse.hpp
+++ include/vmmlib/matrix_pseudoinverse.hpp
@@ -40,16 +40,24 @@ namespace vmml
 				VMMLIB_ERROR( "matrix compute_pseudoinverse - number of matrix rows have to be greater or equal to number of matrix columns.", VMMLIB_HERE );
 			}
 			
+			typedef matrix< T::ROWS, T::COLS, float_t > matrix_mn_t;
+			typedef matrix< T::COLS, T::ROWS, float_t > matrix_nm_t;
+			typedef matrix< T::COLS, T::COLS, float_t > matrix_nn_t;
+			typedef vector< T::COLS, float_t > vec_n_t;
+			typedef vector< T::ROWS, float_t > vec_m_t;
+			typedef matrix< T::COLS, T::ROWS, typename T::value_type> pinv_type;
+			typedef lapack_svd< T::ROWS, T::COLS, float_t > svd_type;
+			typedef blas_dgemm< T::COLS, 1, T::ROWS, float_t > blas_type;
 						
 			// perform an SVD on the matrix to get the singular values
-			lapack_svd< T::ROWS, T::COLS, float_t > svd;
-			matrix< T::ROWS, T::COLS, float_t > U;
-			vector< T::COLS, float_t > sigmas;
-			matrix< T::COLS, T::COLS, float_t > Vt;
-			matrix< T::ROWS, T::COLS, float_t > input_data;
-			input_data.cast_from( input );
+			svd_type* svd = new svd_type;
+			matrix_mn_t* U = new matrix_mn_t;
+			vec_n_t sigmas;
+			matrix_nn_t* Vt = new matrix_nn_t;
+			matrix_mn_t* input_data =  new matrix_mn_t;
+			input_data->cast_from( input );
 			
-			bool ok = svd.compute( input_data, U, sigmas, Vt ); 
+			bool ok = svd->compute( *input_data, *U, sigmas, *Vt ); 
 						
 			
 			if ( !ok ) {
@@ -71,39 +79,47 @@ namespace vmml
 			}
 			
 			//compute inverse with all the significant inverse singular values
-			matrix< T::COLS, T::ROWS, float_t > result;
-			matrix< T::COLS, T::ROWS, typename T::value_type> pseudoinverse;
-			result.zero();
-			matrix< T::COLS, T::ROWS, float_t > tmp;
+			matrix_nm_t* result = new matrix_nm_t;
+			pinv_type* pseudoinverse = new pinv_type;
+			result->zero();
+			matrix_nm_t* tmp = new matrix_nm_t;
 			sigmas.reciprocal();
 			//double sigma_inv = 0;
 
-			vector< T::COLS, float_t> vt_i;
-			vector< T::ROWS, float_t> u_i;
-			blas_dgemm< T::COLS, 1, T::ROWS, float_t > blas_dgemm1;
+			vec_n_t vt_i;
+			vec_m_t u_i;
+			blas_type blas_dgemm1;
 
 			if ( num_sigmas >= 1 ) {
 				
 				it = sigmas.begin();
 				for( size_t i = 0 ;  i < num_sigmas && it != it_end; ++it, ++i ) 
 				{
-					Vt.get_row( i, vt_i);
-					U.get_column( i, u_i );
+					Vt->get_row( i, vt_i);
+					U->get_column( i, u_i );
 
-					blas_dgemm1.compute_vv_outer( vt_i, u_i, tmp );
+					blas_dgemm1.compute_vv_outer( vt_i, u_i, *tmp );
 					
 					//sigma value inverted: 1 / *it;
 					//sigma_inv = (1 / *it );
-					tmp *= *it ;
-					result += tmp;
+					*tmp *= *it ;
+					*result += *tmp;
 					
 				}
-				pseudoinverse.cast_from( result );
-				pseudoinverse.transpose_to( pseudoinverse_transposed );
+				pseudoinverse->cast_from( *result );
+				pseudoinverse->transpose_to( pseudoinverse_transposed );
 				
 			} else {
 				pseudoinverse_transposed.zero(); //return matrix with zeros
 			}
+			
+			delete result;
+			delete tmp;
+			delete Vt;
+			delete U;
+			delete pseudoinverse;
+			delete svd;
+			delete input_data;
 		}
 		
 	}; //end compute_pseudoinverse class

1f421ac89e871be9e2b04ea8ed93fa0e34f9b3e7
susuter <susuter@4d770e62-bb1e-0410-ad62-f2382d3e8790>
2012-02-03 16:29:21 +0000

    - updated pseudomatrix computation (now also for matrices with COLS > ROWS)
    git-svn-id: https://vmmlib.svn.sourceforge.net/svnroot/vmmlib/trunk@577 4d770e62-bb1e-0410-ad62-f2382d3e8790

--- tests/matrix_pseudoinverse_test.cpp
+++ tests/matrix_pseudoinverse_test.cpp
@@ -64,11 +64,11 @@ namespace vmml
 		//FIXME: check if multiply = Identity matrix
 		
 		if (ok) {
-			log( "matrix compute pseudo inverse ", ok  );
+			log( "matrix compute pseudo inverse (ROWS > COLS)", ok  );
 		} else {
 			std::stringstream error;
 			error 
-			<< "matrix compute pseudo inverse: " << std::endl
+			<< "matrix compute pseudo inverse (ROWS > COLS): " << std::endl
 			<< "input 1 is: " << std::endl << input << std::endl
 			<< "inverse matrix 1 should be: " << std::endl << pseudoinverse_control << std::endl
 			<< "inverse matrix 1 is: " << std::endl << pseudoinverse << std::endl
@@ -78,6 +78,43 @@ namespace vmml
 			
 			log_error( error.str() );
 		}
+		
+		//cols > rows
+		typedef matrix< 4, 6, float > pinv_type3;
+		typedef matrix< 6, 4, float > pinv_t_type3;
+		
+		pinv_type3 input3;
+		input3.set( data, data + 24);
+	
+		compute_pseudoinverse< pinv_type3 > compute_pinv3;
+		
+		pinv_type3 pseudoinverse_transposed3;
+		compute_pinv3( input3, pseudoinverse_transposed3 );
+				
+		pinv_type3 pseudoinverse3_control;
+		float data6[] = {
+			0.5110213756561279, 0.1466298252344131, -0.2957937121391296, 0.5260885953903198, 0.08560904860496521, -0.4743495881557465, 
+			-0.6723353862762451, 0.1120621934533119, 0.3330267071723938, 0.5148389935493469, -0.1580927520990372, 0.3453871607780457,
+			1.576845765113831, -0.9367456436157227, 0.3191587626934052, -0.09735386073589325, -0.6063382625579834, -0.04424500837922096,
+			-0.9502310156822205, 0.8770782351493835, -0.1227491199970245, -0.5944689512252808, 0.7954459190368652, 0.3613623976707458	
+		};
+		pseudoinverse3_control.set( data6, data6 + 24);
+		
+		ok = (pseudoinverse3_control.equals( pseudoinverse_transposed3, 1e-6 ));
+		
+		if (ok) {
+			log( "matrix compute pseudo inverse (COLS > ROWS) ", ok  );
+		} else {
+			std::stringstream error;
+			error 
+			<< "matrix compute pseudo inverse (COLS > ROWS): " << std::endl
+			<< "input 3 is: " << std::endl << input3 << std::endl
+			<< "pinv matrix 3 transposed should be: " << std::endl << pseudoinverse3_control << std::endl
+			<< "pinv matrix 3 transposed is: " << std::endl << pseudoinverse_transposed3 << std::endl;
+			
+			log_error( error.str() );
+		}
+		
 		
 		
 		ok = true;

6a76823fe83bad5cc1e4624930e17852ff8216e9
Susanne Suter <susanne.suter@gmail.com>
2012-07-20 07:41:44 -0700

    - added tests for tensor4

--- tests/tensor4_test.cpp
+++ tests/tensor4_test.cpp
@@ -10,25 +10,374 @@ namespace vmml
 	tensor4_test::run()
 	{
 		bool ok = false;
+        // indicates if failing the test produces only a warning
+        bool fail_test = false;
+        
+        // define Tensor
+		const size_t a = 3;
+        const size_t b = 2;
+        const size_t c = 4;
+        const size_t d = 4;
+        typedef int T;
+		tensor4< a, b, c, d, T >  t4;
 		
-		tensor4< 2, 3, 4, 2, int >  t4;
-		t4.zero();
-		
-		if ( ok )
+        // Test size
+        if ( t4.size() == a*b*c*d )
 		{	
-			log( "tensor4 method x", true  );
+			log( "tensor4 method size()", true  );
 		} else
 		{
 			std::stringstream error;
 			error 
-			<< "tensor4 method x. t4 should be: " 
+			<< "tensor4 method size(). size should be: "
+			<< a*b*c*d
+			<< std::endl
+            << "size is: " << t4.size() << std::endl;
+			log_error( error.str(),  fail_test);
+		}
+        
+        // Test filling methods
+        t4.fill(3);
+        ok = true;
+        T* arrayptr = t4.get_array_ptr();
+        for(size_t index = 0; index < t4.size(); ++index)
+        {
+            if( arrayptr[ index ] != 3)
+            {
+                ok = false;
+                break;
+            }
+        }
+        if ( ok )
+		{	
+			log( "tensor4 method fill( value )", true  );
+            
+		} else
+		{
+			std::stringstream error;
+			error 
+			<< "tensor4 method fill( 3 ). t4 is: " 
 			<< std::endl 
 			<< t4
 			<< std::endl;
-			log_error( error.str() );
+			log_error( error.str(),  fail_test);
+		}
+        
+        
+        ok = true;
+        t4 = 6;
+        for(size_t index = 0; index < t4.size(); ++index)
+        {
+            if( arrayptr[ index ] != 6)
+            {
+                ok = false;
+                break;
+            }
+        }
+        if ( ok )
+		{	
+			log( "tensor4 operator=( value )", true  );
+		} else
+		{
+			std::stringstream error;
+			error 
+			<< "tensor4 operator=( 6 ). t4 is: " 
+			<< std::endl 
+			<< t4
+			<< std::endl;
+			log_error( error.str(),  fail_test);
+		}
+        
+        
+        t4.fill_increasing_values();
+        ok = true;
+        for(size_t index = 0; index < t4.size(); ++index)
+        {
+            if( arrayptr[ index ] != index)
+            {
+                ok = false;
+                break;
+            }
+        }
+        
+        if ( ok )
+		{	
+			log( "tensor4 method fill_increasing()", true  );
+		} else
+		{
+			std::stringstream error;
+			error 
+			<< "tensor4 method fill_increasing(). t4 is: " 
+			<< std::endl 
+			<< t4
+			<< std::endl;
+			log_error( error.str(),  fail_test);
+		}
+        ok = false;
+        
+        // Test indexing ( at and () )
+        
+        // test const versions as well
+        const int c1 = t4(1,0,0,0);
+        const int clast = t4.at(a-1,b-1,c-1,d-1);
+        if ( t4(0,0,0,0) == 0 && t4.at(2, 1, 2, 2) == 2*a*b*c + 2*a*b + 1*a + 2 && c1 == 1 && clast == t4.size() - 1 )
+		{	
+			log( "tensor4 indexing", true  );
+		} else
+		{
+			std::stringstream error;
+			error 
+			<< "tensor4 indexing. t4 values should be: " 
+			<< std::endl 
+			<< 0 << "\t" << 2*a*b*c+2*a*b+a+2 << "\t" << 1 << "\t" << t4.size() - 1
+			<< std::endl
+            << "t4 values are: " << std::endl
+            << t4(0,0,0,0) << "\t" << t4.at(2, 1, 2, 2) << "\t" << c1 << "\t" << clast << std::endl;
+            
+			log_error( error.str(),  fail_test);
+		}
+        
+        // Test equality
+        tensor4<a, b, c, d, T> t4cp;
+        t4.fill_increasing_values();
+        t4cp.fill_increasing_values();
+        tensor4<a, b, c, d, T> t4mod;
+        t4mod.fill_increasing_values();
+        t4mod(0,0,1,0) = 1;
+        if ( t4 == t4cp && ! (t4 != t4cp) && t4mod != t4 && !(t4mod == t4))
+		{	
+			log( "tensor4 equality operators", true  );
+		} else
+		{
+			std::stringstream error;
+			error 
+			<< "tensor4 equality operators. Results should be: " 
+			<< std::endl 
+			<< true << true << true << true
+			<< std::endl
+            << "Results are:" << std::endl
+            << (t4 == t4cp) << (! (t4 != t4cp)) << (t4mod != t4) << (! (t4mod == t4)) << std::endl;
+			log_error( error.str(),  fail_test);
+		}
+        
+        
+        // Test copy ctors
+        tensor4< a, b, c, d, T > t4tempcp(t4);
+        
+        if ( t4tempcp == t4 )
+		{	
+			log( "tensor4 copy constructor", true  );
+		} else
+		{
+			std::stringstream error;
+			error 
+			<< "tensor4 copy constructor. t4 is: " 
+			<< std::endl 
+			<< t4
+			<< std::endl
+            << "tensor4 copy constructor. t4 copy is: "
+            << std::endl 
+			<< t4tempcp
+			<< std::endl;
+			log_error( error.str(),  fail_test);
 		}
 		
+        
+        // cast constructor
+        tensor4< a, b, c, d, long > t4long(t4);
 		
+        
+        T* t4ptr = t4.get_array_ptr();
+        long* t4longptr = t4long.get_array_ptr();
+        ok = true;
+        for(size_t index = 0; index < t4long.size() && ok; index++)
+        {
+            if((long)t4ptr[ index ] != t4longptr[ index ])
+            {
+                ok = false;
+            }
+        }
+        
+        if ( ok )
+		{	
+			log( "tensor4 type converter constructor", true  );
+		} else
+		{
+			std::stringstream error;
+			error 
+			<< "tensor4 type converter constructor. t4 is: " 
+			<< std::endl 
+			<< t4
+			<< std::endl
+            << "tensor4 type converter constructor. t4 in long is: " 
+			<< std::endl 
+			<< t4long
+			<< std::endl;
+			log_error( error.str(),  fail_test);
+		}
+        
+        ok = true;
+        // resize constructor
+        tensor4< a+2, b+2, c+2, d-2, T > t4resized(t4);
+        T target = 0;
+        for(size_t i4 = 0; i4 < t4resized.T3S; ++i4)
+        {
+            for(size_t i3 = 0; i3 < t4resized.SLICES; ++i3)
+            {
+                for(size_t i2 = 0; i2 < t4resized.COLS; ++i2)
+                {
+                    for(size_t i1 = 0; i1 < t4resized.ROWS; ++i1)
+                    {
+                        if(i1 < t4.ROWS && i2 < t4.COLS && i3 < t4.SLICES && i4 < t4.T3S )
+                        {
+                            target = t4(i1, i2, i3, i4);
+                        }else
+                        {
+                            target = 0;
+                        }
+                        
+                        if(t4resized(i1, i2, i3, i4) != target)
+                        {
+                            ok = false;
+                        }
+                    }
+                }
+            }
+        }
+        
+		if ( ok )
+		{	
+			log( "tensor4 resize constructor", true  );
+		} else
+		{
+			std::stringstream error;
+			error 
+			<< "tensor4 resize constructor. t4 is: " 
+			<< std::endl 
+			<< t4
+			<< std::endl
+            << "tensor4 resize constructor. t4 resized is: " 
+			<< std::endl 
+			<< t4resized
+			<< std::endl;
+			log_error( error.str(),  fail_test);
+		}
+        ok = false;
+        
+        tensor4< a, b, c, d, T > t4assigned;
+        t4assigned = t4;
+        t4assigned = t4assigned; // also check self assignment
+        
+        if ( ( (&t4assigned) != (&t4) ) && t4assigned == t4 )
+		{
+			log( "tensor4 assignment operator=", true  );
+		} else
+		{
+			std::stringstream error;
+			error
+			<< "tensor4 assignment operator=. t4 is: " 
+			<< std::endl 
+			<< t4 << " at " << &t4
+			<< std::endl
+            << "tensor4 assignment operator=. assigned t4 is: " 
+			<< std::endl 
+			<< t4assigned << " at " << &t4assigned
+			<< std::endl;
+			log_error( error.str(),  fail_test);
+		}
+        
+		
+		// fill random plausability tests
+        t4.fill_random();
+        t4ptr = t4.get_array_ptr();
+        bool fail = true;
+        T min = t4ptr[ 0 ];
+        T max = t4ptr[ 0 ];
+        for(size_t index = 0; index < t4.size(); index++)
+        {
+            if(t4ptr[ index ] < min)
+            {
+                min = t4ptr[ index ];
+            }else if(t4ptr [ index ] > max)
+            {
+                max = t4ptr[ index ];
+            }
+            
+            if(index > 0 && t4ptr[ index ] != t4ptr[ index-1 ]) // not all values can be identical
+            {
+                fail = false;
+            }
+        }
+        
+        if ( !fail && max - min > std::numeric_limits< T >::max()/4) // assert at least a certain bandwith of values
+		{
+			log( "tensor4 random fill", true  );
+		} else
+		{
+			std::stringstream error;
+			error
+			<< "tensor4 random fill. t4 is: " 
+			<< std::endl 
+			<< t4
+			<< std::endl;
+            
+            if(fail) // if all values are the same, fail the test
+            {
+                log_error( error.str(),  fail_test);
+            }else
+            {
+                log_error( error.str(),  !fail_test);
+            }
+		}
+        
+        t4.fill_random_signed();
+        t4ptr = t4.get_array_ptr();
+        fail = true;
+        min = 0;
+        max = 0;
+        for(size_t index = 0; index < t4.size(); index++)
+        {
+            if(t4ptr[ index ] < min)
+            {
+                min = t4ptr[ index ];
+            }else if(t4ptr [ index ] > max)
+            {
+                max = t4ptr[ index ];
+            }
+            
+            if(index > 0 && t4ptr[ index ] != t4ptr[ index-1 ]) // not all values can be identical
+            {
+                fail = false;
+            }
+        }
+        
+        if(min == 0 || max == 0 ) // at least one positive and one negative number
+        {
+            fail = true;
+        }
+        
+        if ( !fail && max - min > std::numeric_limits< T >::max()/2) // assert at least a certain bandwith of values
+		{
+			log( "tensor4 random fill signed", true  );
+		} else
+		{
+			std::stringstream error;
+			error
+			<< "tensor4 random fill signed. t4 is: " 
+			<< std::endl 
+			<< t4
+			<< std::endl;
+            
+            if(fail) // if all values are the same or no negative/positive numbers, fail the test
+            {
+                log_error( error.str(),  fail_test);
+            }else
+            {
+                log_error( error.str(),  !fail_test);
+            }
+		}
+        
+ 
 		return ok;
 	}
 	

9439525c76f771665c2a600c7556fea8474335b8
Jonas Boesch <boesch@ifi.uzh.ch>
2012-07-20 11:45:12 +0200

    fixed and refactored unit test base class

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -5,6 +5,9 @@ namespace vmml
 
 unit_test::unit_test( const std::string& test_name )
     : _tolerance( 1e-9 )
+    , _ok_prefix(   "[  ok  ]" )
+    , _warn_prefix( "[ WARN ]" )
+    , _fail_prefix( "[ FAIL ]" )
 {
 	_log = "UNIT TEST: ";
 	_log += test_name;
@@ -14,27 +17,56 @@ unit_test::unit_test( const std::string& test_name )
 
 
 void
-unit_test::log( const std::string& event, bool status_ok, bool warning_only )
+unit_test::log( const std::string& msg, bool status_ok, bool warning_only )
 {
-	if ( status_ok )
-		_log += "[  ok  ] ";
-	else if ( ! warning_only )
-		_log += "[ FAIL ] ";
-	else
-		_log += "[ WARN ] ";	
-	
-	_log += event;
+    if ( status_ok )
+        _log += _ok_prefix;
+    else 
+        _log += warning_only ? _warn_prefix : _fail_prefix;
+    
+	_log += " ";
+	_log += msg;
 	_log += "\n";
 }
 
 
 
 void
-unit_test::log_error( const std::string& error_msg, bool warning_only  )
+unit_test::log_error( const std::string& msg, bool warning_only  )
 {
-	_log += "[ WARN ] ";
-	_log += error_msg;
-	_log += "\n";
+    log(msg, false, warning_only);
+}
+
+
+
+void
+unit_test::set_prefixes( const std::string ok_, const std::string& warn_, 
+    const std::string& fail_ ) 
+{
+    _ok_prefix      = ok_;
+    _warn_prefix    = warn_;
+    _fail_prefix    = fail_;
+}
+
+    
+const std::string&
+unit_test::get_fail_prefix()
+{
+    return _fail_prefix;
+}
+
+
+const std::string&
+unit_test::get_warn_prefix()
+{
+    return _warn_prefix;
+}
+
+
+const std::string&
+unit_test::get_ok_prefix()
+{
+    return _ok_prefix;
 }
 
 } // namespace vmml
--- tests/unit_test.hpp
+++ tests/unit_test.hpp
@@ -21,12 +21,24 @@ public:
 		os << unit_test_._log;
 		return os;
 	}
+    
+    void set_prefixes(  const std::string ok_       = "[  ok  ]", 
+                        const std::string& warn_    = "[ WARN ]", 
+                        const std::string& fail_    = "[ FAIL ]" );
+        
+    const std::string& get_fail_prefix();
+    const std::string& get_warn_prefix();
+    const std::string& get_ok_prefix();
 
 protected:
-	virtual void log( const std::string& event, bool status_ok, bool warning_only = false );
-	virtual void log_error( const std::string& error_msg, bool warning_only = false );
+	virtual void log( const std::string& msg, bool status_ok, bool warning_only = false );
+	virtual void log_error( const std::string& msg, bool warning_only = false );
        
 	std::string _log;
+    
+    std::string _fail_prefix;
+    std::string _warn_prefix;
+    std::string _ok_prefix;
 
     double  _tolerance;
 

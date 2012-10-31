ca2e8b574a64a432bae6d0eaed7a085a5a86097f
Susanne Suter <susanne.suter@gmail.com>
2012-10-31 10:31:51 -0700

    - t3_padder, implemented cached batch reading

--- include/vmmlib/t3_padder.hpp
+++ include/vmmlib/t3_padder.hpp
@@ -25,6 +25,8 @@ namespace vmml
 		typedef tensor3< I, I, I, T > t3_pow2_t;
 		typedef tensor3< B, B, B, T > t3_block_t;
 		typedef tensor3< I1, I2, B, T > t3_cached_t;
+		
+		typedef typename vmml::tensor3_iterator< t3_cached_t >	         iterator;
 
 
 		t3_padder( const std::string& dir_, const std::string& filename_ );  
@@ -32,8 +34,8 @@ namespace vmml
 		~t3_padder();
 		
 		void get_data_block( t3_block_t& t3_block_, size_t j1_, size_t j2_, size_t j3_ );
-		void read_from_raw( const size_t j3_ );
-		
+		void read_from_raw();
+
 		
 	private:
 		std::string _path;
@@ -62,7 +64,7 @@ namespace vmml
 			VMMLIB_ERROR( "t3_padder - no input file open", VMMLIB_HERE );
 		}
 		
-		read_from_raw( _cache_idx );
+		read_from_raw();
 	}
 	
 
@@ -76,51 +78,96 @@ namespace vmml
 	void
 	VMML_TEMPLATE_CLASSNAME::get_data_block( t3_block_t& t3_block_, size_t j1_, size_t j2_, size_t j3_ )
 	{
-		//check if data at j3_ index was already cached
 		
-		if ( _cache_idx == j3_ )
-		{
-			//do nothing
-		}
-		else
-		{
-			//if no, read all slices for given J3 range, j3_ until j3_+B
-			read_from_raw( j3_ );
-		}
-		
-		// if block is within data area -> return respective block
-		if( j1_ < I1 && j2_ < I2 && j3_ < I3 && (j1_ + B) < I1 && (j2_ + B) < I2 && (j3_ + B) < I3 )
-		{
-			_cached_data.get_sub_tensor3( t3_block_, j1_, j2_ );
-		}
 		// if block is outside data area -> return zero-padded block
-		else if( j1_ >= I1 && j2_ >= I2 && j3_ >= I3 )
+		if( j1_ >= I1 && j2_ >= I2 && j3_ >= I3 )
 		{
 			t3_block_.zero();
 		}
-		// if block is partly outside data area -> return data block zero-padded
-		else 
-		{
-			t3_block_.zero();
-			//FIXME: check sizes
-			//Maybe do it withouth templates
-			const size_t k1 = 1; //B - (I - I1 - j1_ )
-			const size_t k2 = 1;
-			const size_t k3 = 1;
-			//std::cout << "subvolume: (" << k1 << "," << k2 << "," << k3 << ")" << std::endl;
-			tensor3< k1, k2, k3, T > t3_sub_block;
-			_cached_data.get_sub_tensor3( t3_sub_block, j1_, j2_ );
-			t3_block_.set_sub_tensor3( t3_sub_block, B-k1, B-k2, B-k3 );
+		else {
+			//check if data at j3_ index was already cached
+			if ( _cache_idx == j3_ )
+			{
+				//do nothing
+			}
+			else
+			{
+				//if no, read all slices for given J3 range, j3_ until j3_+B
+				_cache_idx = j3_;
+				read_from_raw();
+			}
+			
+			// if block is within data area -> return respective block
+			if( j1_ < I1 && j2_ < I2 && j3_ < I3 && (j1_ + B) < I1 && (j2_ + B) < I2 && (j3_ + B) < I3 )
+			{
+				_cached_data.get_sub_tensor3( t3_block_, j1_, j2_ );
+			}
+			// if block is partly outside data area -> return data block zero-padded
+			else 
+			{
+				t3_block_.zero();
+				//FIXME: check sizes
+				//Maybe do it withouth templates
+				const size_t k1 = 1; //B - (I - I1 - j1_ )
+				const size_t k2 = 1;
+				const size_t k3 = 1;
+				//std::cout << "subvolume: (" << k1 << "," << k2 << "," << k3 << ")" << std::endl;
+				tensor3< k1, k2, k3, T > t3_sub_block;
+				_cached_data.get_sub_tensor3( t3_sub_block, j1_, j2_ );
+				t3_block_.set_sub_tensor3( t3_sub_block, B-k1-1, B-k2-1, B-k3-1 );
+			}
 		}
 
 	}
 	
 	VMML_TEMPLATE_STRING
 	void
-	VMML_TEMPLATE_CLASSNAME::read_from_raw( const size_t j3_ )
+	VMML_TEMPLATE_CLASSNAME::read_from_raw()
 	{
-		//FIXME: replace with actual reading
-		_cached_data.fill_increasing_values();
+		//FIXME: if data is shorter than z-dir, update size of read data
+		//out of bounds handling
+		
+		size_t max_file_len = 2147483648u - sizeof(T) ;
+		size_t cache_data_len = I1 * I2 * B * sizeof(T);
+		size_t len_read = 0;
+		size_t start_idx = _cache_idx * I1 * I2 * sizeof(T);
+		char* data = new char[ cache_data_len ];
+		std::ifstream infile;
+		infile.open( _path.c_str(), std::ios::in);
+		
+		if( infile.is_open())
+		{
+			infile.seekg( start_idx, std::ios::beg );
+			if( infile.tellg() != start_idx )
+			{
+				std::cout << "Can't proceed to the offset: " << start_idx << " to read file: " << _path.c_str() << std::endl;
+				infile.close();
+			}
+			
+			infile.read( &data[0], cache_data_len );
+			
+			iterator  it = _cached_data.begin(),
+			it_end = _cached_data.end();
+			
+			while ( cache_data_len > 0 )
+			{
+				len_read = (cache_data_len % max_file_len ) > 0 ? cache_data_len % max_file_len : cache_data_len;
+				cache_data_len -= len_read;
+				
+				T* T_ptr = (T*)&(data[0]);
+				for( ; (it != it_end) && (len_read > 0); ++it, len_read -= sizeof(T) )
+				{
+					*it = *T_ptr; ++T_ptr;
+				}
+			}
+			
+			delete[] data;
+			infile.close();
+		} else {
+			std::cout << "no file open" << std::endl;
+		}
+		//std::cout << "read file at z index: " << _cache_idx << "\n" << _cached_data << std::endl;
+		
 	}
 	
 	

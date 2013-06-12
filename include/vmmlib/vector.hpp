68dcb7cc9290548279d10f7ea9621ebabbd36301
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-06-12 15:09:13 +0200

    Simplify Vector::operator ==

--- include/vmmlib/vector.hpp
+++ include/vmmlib/vector.hpp
@@ -1362,15 +1362,13 @@ vector< M, T >::project_point_onto_plane( const vector< 3, TT >& point,
 
 
 template< size_t M, typename T >
-bool
-vector< M, T >::operator==( const vector< M, T >& other ) const
+bool vector< M, T >::operator==( const vector< M, T >& other ) const
 {
-    bool is_ok = true;
-    for( size_t index = 0; is_ok && index < M; ++index )
-    {
-        is_ok = at( index ) == other.at( index );
-    }
-    return is_ok;
+    for( size_t i = 0; i < M; ++i )
+        if( at( i ) != other.at( i ))
+            return false;
+
+    return true;
 }
 
 

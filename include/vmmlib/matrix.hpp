VMML/vmmlib

commit 06942b2891a3f6ac015e88ca9c4c0b7f5625d166

Sanitize Matrix::get_translation API

include/vmmlib/matrix.hpp
@@ -362,11 +362,12 @@ public:
 
     template< typename TT >
     matrix< M, N, T >& set_translation( const vector< 3, TT >& t,
-                         typename enable_if< M == N && M == 4, TT >::type* = 0 );
+                        typename enable_if< M == N && M == 4, TT >::type* = 0 );
 
     template< typename TT >
-    void get_translation( vector< 3, TT >& translation_,
-        typename enable_if< M == N && M == 4, TT >::type* = 0 ) const;
+    void get_translation( vector< N-1, TT >& translation_ ) const;
+
+    vector< N-1, T > get_translation() const;
 
     // hack for static-member-init
     template< typename init_functor_t >
@@ -2096,16 +2097,21 @@ inline matrix< M, N, T >& matrix< M, N, T >::set_translation(
     return set_translation( translation_.array );
 }
 
-template< size_t M, size_t N, typename T >
-template< typename TT >
-inline void
-matrix< M, N, T >::
-get_translation( vector< 3, TT >& translation_,
-    typename enable_if< M == N && M == 4, TT >::type* ) const
+template< size_t M, size_t N, typename T > template< typename TT > inline
+void matrix< M, N, T >::get_translation( vector< N-1, TT >& translation_ )
+    const
 {
-    translation_.array[ 0 ] = array[ 12 ];
-    translation_.array[ 1 ] = array[ 13 ];
-    translation_.array[ 2 ] = array[ 14 ];
+    for( size_t i = 0; i < N-1; ++i )
+        translation_.array[ i ] = array[ i * M ];
+}
+
+
+template< size_t M, size_t N, typename T >
+inline vector< N-1, T > matrix< M, N, T >::get_translation() const
+{
+    vector< N-1, T > result;
+    get_translation( result );
+    return result;
 }
 
 

tests/matrix_test.cpp
@@ -676,28 +676,30 @@ bool matrix_test::run()
         }
     }
 
-    // set( .... )
+    // set( .... ), _translation()
     {
         ok = true;
 
-        double mData[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        double mData[] = {  0,  1,  2,  3,
+                            4,  5,  6,  7,
+                            8,  9, 10, 11,
+                           12, 13, 14, 15 };
 
         matrix< 4, 4, double > m4x4;
         matrix< 4, 4, double > m4x4C;
+
         m4x4C = mData;
-
         m4x4.set( mData, mData + 16, true );
+        TEST( m4x4 == m4x4C );
 
-        TEST(m4x4 == m4x4C);
+        const vector< 3, double > trans( 3, 7, 11 );
+        TEST( m4x4.get_translation() == trans );
 
         m4x4C = transpose( m4x4C );
-
         m4x4.set( mData, mData + 16, false );
-
-        TEST(m4x4 == m4x4C);
+        TEST( m4x4 == m4x4C );
 
         log( "set( iterator_t, bool ) ", ok );
-
     }
 
     // test direct sum

.travis.yml
@@ -6,20 +6,22 @@ language: cpp
 script:
  - mkdir Debug
  - cd Debug
- - cmake .. -DCI_BUILD_COMMIT=$TRAVIS_COMMIT -DCMAKE_BUILD_TYPE=Debug
+ - cmake .. -DCI_BUILD_COMMIT=$TRAVIS_COMMIT -DCMAKE_BUILD_TYPE=Debug -DTRAVIS=1
  - env TRAVIS=1 make -j8 tests ARGS=-V
  - mkdir ../Release
  - cd ../Release
- - cmake .. -DCI_BUILD_COMMIT=$TRAVIS_COMMIT -DCMAKE_BUILD_TYPE=Release
+ - cmake .. -DCI_BUILD_COMMIT=$TRAVIS_COMMIT -DCMAKE_BUILD_TYPE=Release -DTRAVIS=1
  - env TRAVIS=1 make -j8 tests ARGS=-V
 before_install:
  - sudo apt-get update -qq
  - sudo apt-get install -qq autoconf || /bin/true
  - sudo apt-get install -qq automake || /bin/true
  - sudo apt-get install -qq cmake || /bin/true
+ - sudo apt-get install -qq doxygen || /bin/true
  - sudo apt-get install -qq git || /bin/true
  - sudo apt-get install -qq git-review || /bin/true
  - sudo apt-get install -qq git-svn || /bin/true
+ - sudo apt-get install -qq lcov || /bin/true
  - sudo apt-get install -qq ninja-build || /bin/true
  - sudo apt-get install -qq pkg-config || /bin/true
  - sudo apt-get install -qq subversion || /bin/true

CMake/FindPackages.cmake
@@ -35,7 +35,7 @@ if(OpenMP_name)
   endif()
 endif()
 
-set(VMMLIB_BUILD_DEBS autoconf;automake;cmake;git;git-review;git-svn;ninja-build;pkg-config;subversion)
+set(VMMLIB_BUILD_DEBS autoconf;automake;cmake;doxygen;git;git-review;git-svn;lcov;ninja-build;pkg-config;subversion)
 
 set(VMMLIB_DEPENDS OpenMP)
 

include/vmmlib/vector.hpp
@@ -193,8 +193,7 @@ public:
     vector( const vector< N, T >& source_,
             typename enable_if< N == M + 1 >::type* = 0  );
 
-    template< typename U >
-    vector( const vector< M, U >& source_ );
+    template< typename U > vector( const vector< M, U >& source_ );
 
     void set( T a ); // sets all components to a;
 #ifndef SWIG

commit 8ec1b53ab90396e8d91adae847f7db2279265074

Fixing a regression in 06942b2891a3f6ac015e88ca9c4c0b7f5625d166

include/vmmlib/matrix.hpp
@@ -2102,7 +2102,7 @@ void matrix< M, N, T >::get_translation( vector< N-1, TT >& translation_ )
     const
 {
     for( size_t i = 0; i < N-1; ++i )
-        translation_.array[ i ] = array[ i * M ];
+        translation_.array[ i ] = array[ M * (N - 1) + i ];
 }
 
 

tests/matrix_test.cpp
@@ -692,7 +692,7 @@ bool matrix_test::run()
         m4x4.set( mData, mData + 16, true );
         TEST( m4x4 == m4x4C );
 
-        const vector< 3, double > trans( 3, 7, 11 );
+        const vector< 3, double > trans( 12, 13, 14 );
         TEST( m4x4.get_translation() == trans );
 
         m4x4C = transpose( m4x4C );
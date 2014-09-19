5468ba64a253ad85f2543760098d9e6fe5223a66
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-09-19 14:01:00 +0200

    Update generated files

--- CMake/vmmlib.cmake
+++ CMake/vmmlib.cmake
@@ -2,7 +2,8 @@
 
 set(VMMLIB_PACKAGE_VERSION 1.7.0)
 set(VMMLIB_REPO_URL https://github.com/Eyescale/vmmlib.git)
-set(VMMLIB_DEPENDS OPTIONAL OpenMP LAPACK CBLAS)
+set(VMMLIB_DEPENDS OPTIONAL OpenMP Boost)
+set(VMMLIB_BOOST_COMPONENTS "unit_test_framework")
 
 if(CI_BUILD_COMMIT)
   set(VMMLIB_REPO_TAG ${CI_BUILD_COMMIT})
